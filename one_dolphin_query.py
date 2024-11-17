616a70f6a22e35d546c891dbdfcb865cca92249e
Grace <64219828+graceful-one@users.noreply.github.com>
2024-11-17 08:59:03 -0500

    trying to read in dfs

--- one_dolphin_query.py
+++ one_dolphin_query.py
@@ -1,10 +1,18 @@
-## Create dataset as dataframe  
+## install packages
+import pandas as pd 
+#import geopandas as gpd
 
-## Set up dictionaries of meanings 
+##load dolphin database
+dolphin_df = pd.read_csv('../tblDolphin.csv')
+observations_df = pd.read_csv('../tblDolphinsObserved.csv')
+
+## Set up dictionaries of meanings - idk if i actually need to do this
 
 ## Take in user input of which dolphin  
+search_dolphin = "4445a" # hard coding for now will figure out the input later
 
 ## Search dataframe of observations for all sightings of that dolphin  
+
 
 ## Create new dataframe of the dates of those sightings  
 
