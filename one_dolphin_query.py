9eb9a94d60385f445434c5e87ddcc419b0b0cce9
Grace <64219828+graceful-one@users.noreply.github.com>
2024-11-17 11:26:31 -0500

    Update one_dolphin_query.py

--- one_dolphin_query.py
+++ one_dolphin_query.py
@@ -1,10 +1,18 @@
 ## install packages
+import streamlit as st
 import pandas as pd 
-#import geopandas as gpd
+
+#import geopandas as gpd 
+st.title("My new Streamlit app")
 
 ##load dolphin database
-dolphin_df = pd.read_csv('../tblDolphin.csv')
-observations_df = pd.read_csv('../tblDolphinsObserved.csv')
+try:
+    dolphin_df = pd.read_csv(r"/workspaces/859-Final-Project/tblDolphin.csv", encoding="ISO-8859-1")
+    st.write(dolphin_df.head())
+except UnicodeDecodeError:
+    st.error("There was an error reading the file due to encoding issues.")
+except FileNotFoundError:
+    st.error("The file was not found at the specified path.")
 
 ## Set up dictionaries of meanings - idk if i actually need to do this
 
@@ -19,3 +27,4 @@ search_dolphin = "4445a" # hard coding for now will figure out the input later
 ## Print basic dolphin information  
 
 ## Print out table of dates of observations 
+
